fddbdb8dbafdc46a61877b813fb14424e41daee2
pktealshift <tealshift@pkfitness.com>
2023-08-25 17:45:29 -0700

    Restored BVUserDefault protocol

--- Sources/BVFoundation/Tools.swift
+++ Sources/BVFoundation/Tools.swift
@@ -1,122 +1,16 @@
 //
 //  Tools.swift
-//  
+//
 //
 //  Created by TealShift Schwifty on 8/31/22.
 //
 
 import Foundation
-import Combine
-//public protocol BVUserDefault {
-//    var key: String { get }
-//    var value: Any? { get }
-//    func update(_ value: Any?)
-//}
 
-@propertyWrapper public final class UserPref<T>: NSObject {
-    private let key: String
-    private let userDefaults: UserDefaults
-    private var observerContext = 0
-    private let subject: CurrentValueSubject<T, Never>
-    private let syncBlock: (String)->()
-
-    /// Initialize a key/value to track
-    /// - Parameters:
-    ///   - defaultValue: Default preference value
-    ///   - key: Preference key to track
-    ///   - storage: UserDefaults containing value for preference
-    ///   - sync: Delegate responsible for keeping this in sync with other platforms
-    public init(wrappedValue defaultValue: T, _ key: String,
-                storage: UserDefaults = .standard, _ sync: @escaping (String)->() = {_ in }) {
-        self.key = key
-        self.subject = CurrentValueSubject(defaultValue)
-        self.userDefaults = storage
-        self.syncBlock = sync
-        super.init()
-        userDefaults.register(defaults: [key: defaultValue])
-        /// The publisher is only called when the value is updated
-        userDefaults.addObserver(self, forKeyPath: key, options: .new, context: &observerContext)
-        subject.value = wrappedValue
-    }
-
-    public override func observeValue(forKeyPath keyPath: String?, of object: Any?,
-                               change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if context == &observerContext {
-            subject.value = wrappedValue
-            syncBlock(key)
-        } else {
-            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
-        }
-    }
-    
-    public var wrappedValue: T {
-        get { userDefaults.object(forKey: key) as? T ?? subject.value }
-        set {
-            Log.v("Set \(key) -> \(newValue)")
-            userDefaults.setValue(newValue, forKey: key)
-        }
-    }
-    public var projectedValue: AnyPublisher<T, Never> {
-        subject.eraseToAnyPublisher()
-    }
-
-    deinit {
-        userDefaults.removeObserver(self, forKeyPath: key, context: &observerContext)
-    }
-}
-
-@propertyWrapper public final class UserPrefOptional<T>: NSObject {
-    private var key: String
-    private let userDefaults: UserDefaults
-    private var observerContext = 0
-    private let subject: CurrentValueSubject<T?, Never>
-    private let syncBlock: (String)->()
-    
-    /// Initialize a key/value to track
-    /// - Parameters:
-    ///   - defaultValue: Default preference value
-    ///   - key: Preference key to track
-    ///   - storage: UserDefaults containing value for preference
-    ///   - sync: Delegate responsible for keeping this in sync with other platforms
-    public init(wrappedValue defaultValue: T? = nil, _ key: String,
-                storage: UserDefaults = .standard, _ sync: @escaping (String)->() = {_ in }) {
-        self.key = key
-        self.subject = CurrentValueSubject(defaultValue)
-        self.userDefaults = storage
-        self.syncBlock = sync
-        super.init()
-        if let defaultValue = defaultValue {
-            userDefaults.register(defaults: [key: defaultValue])
-        }
-        /// The publisher is only called when the value is updated
-        userDefaults.addObserver(self, forKeyPath: key, options: .new, context: &observerContext)
-        subject.value = wrappedValue
-    }
-
-    public override func observeValue(forKeyPath keyPath: String?, of object: Any?,
-                               change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if context == &observerContext {
-            subject.value = wrappedValue
-            syncBlock(key)
-        } else {
-            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
-        }
-    }
-    
-    public var wrappedValue: T? {
-        get { userDefaults.object(forKey: key) as? T }
-        set {
-            Log.v("Set \(key) -> \(String(describing: newValue))")
-            userDefaults.setValue(newValue, forKey: key)
-        }
-    }
-    public var projectedValue: AnyPublisher<T?, Never> {
-        subject.eraseToAnyPublisher()
-    }
-
-    deinit {
-        userDefaults.removeObserver(self, forKeyPath: key, context: &observerContext)
-    }
+public protocol BVUserDefault {
+    var key: String { get }
+    var value: Any? { get }
+    func update(_ value: Any?)
 }
 
 public extension Sequence {
